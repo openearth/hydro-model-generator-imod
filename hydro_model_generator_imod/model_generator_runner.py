4f7ac06b5c5a9354be7dd22f2f58e5ffaa5ff6ab
quanpands <50743015+quanpands@users.noreply.github.com>
2019-08-06 14:29:28 +0200

    Update model_generator_runner.py
    test-20190806

--- hydro_model_generator_imod/model_generator_runner.py
+++ hydro_model_generator_imod/model_generator_runner.py
@@ -1,10 +1,11 @@
-import os
+import os, sys
 import shutil
 import yaml
 import zipfile
 import hydroengine
 import numpy as np
 import logging
+import json
 
 from pathlib import Path
 from shapely import geometry as sg
@@ -37,11 +38,11 @@ def utm_epsg(region):
     return epsg
 
 
-def get_hydro_data(region, ds):
+def get_hydro_data(region, ds, model_id):
     logging.info(ds["path"] + " \n  \n  " + ds["variable"] + "    ")
+    ds["path"] = os.path.join(Path(ds["path"]).parent, model_id, Path(ds["path"]).name)
+    Path(ds["path"]).parent.mkdir(parents=True, exist_ok=True)
     if ds["function"] == "get-raster" and ds["source"] == "earth-engine":
-        # create directory
-        Path(ds["path"]).parent.mkdir(parents=True, exist_ok=True)
         if ds["crs"].lower() == "utm":
             ds["crs"] = "EPSG:{}".format(utm_epsg(region))
         hydroengine.download_raster(
@@ -58,9 +59,20 @@ def get_hydro_data(region, ds):
     elif ds["function"] == "get-rivers" and ds["source"] == "earth-engine":
         filter_upstream_gt = 1000
         hydroengine.download_rivers(region, ds["path"], filter_upstream_gt, ds["region_filter"], ds["catchment_level"])
+    elif ds["function"] == "get-features":
+        download_features(region, ds["source"], ds["path"])
+    else:
+        raise ValueError(f"Invalid function provided for {ds['variable']}.")
+
+def download_features(region, source, path):
+    """Downloads feature collection to JSON file"""
+    # TODO remove in due time, when part of hydroengine
+    feature_collection = hydroengine.get_feature_collection(region, source)
+    with open(path, "w") as f:
+        json.dump(feature_collection, f)
 
 
-def general_options(d):
+def general_options(d, model_id):
     # get data from hydro-engine one by one
     defaults = d["hydro-engine"]["defaults"]
     for ds_override in d["hydro-engine"]["datasets"]:
@@ -68,7 +80,11 @@ def general_options(d):
         ds.update(ds_override)
         print("=> general_options.hydro-engine.datasets.variable:", ds["variable"])
         if ds["source"] == "earth-engine":
-            get_hydro_data(d["region"], ds)
+            get_hydro_data(d["region"], ds, model_id)
+        elif "huite" in ds["source"]:
+            get_hydro_data(d["region"], ds, model_id)
+        elif "USDOS" in ds["source"]:
+            get_hydro_data(d["region"], ds, model_id)
         else:
             # TODO support non earth-engine datasets
             print("skipped variable:", ds["variable"])
@@ -91,10 +107,10 @@ def zip_model_output(input_dir, output_dir):
     zipf.close()
     return zipf
 
-def delete_output_files(input_dir):
+def delete_output_files(input_dir, model_id):
     shutil.rmtree(input_dir)
     
-    origfolder = "hydro-engine/"
+    origfolder = "/app/hydro-engine/iMOD/{}/".format(model_id)
     for item in os.listdir(origfolder):
         if item.endswith(".tif"):
             os.remove(os.path.join(origfolder, item))
@@ -106,32 +122,32 @@ def delete_output_files(input_dir):
             os.remove(os.path.join(origfolder, item))
     # pass
 
-def main():
-    path = 'imodflow_onlyHE.yaml'
+def main(model_id):
+    path = '/app/hydro-generator/yaml/iMOD-{}.yaml'.format(model_id)
     # parse yaml (hydro_model_builder.parse_config)
     dicts = parse_config(path)
     genopt, modopt = dicts
     # get hydro data (hydro_model_builder.general_options)
-    general_options(genopt)
+    general_options(genopt, model_id)
     # run genwf = ModelGeneratorWflow() genwf.generate_model(genopt, modopt)
     # genwf = ModelGeneratorWflow()
     # genwf.generate_model(genopt, modopt)
-    model_builder.fetch_data(genopt)
-    model_generator.build_model(**modopt, general_options=genopt)
+    # model_builder.fetch_data(genopt)
+    model_generator.build_model(**modopt, general_options=genopt, model_id=model_id)
 
     # TODO: add relative paths
-    input_dir = modopt['modelname']
-    output_dir = modopt['modelname'] + '.zip'
+    input_dir = '/app/hydro-input/{}-{}'.format(modopt['modelname'], model_id)
+    output_dir = '/app/hydro-input/{}-{}.zip'.format(modopt['modelname'], model_id)
     # output_dir = "{0}-{1}.zip".format(model['type'], model['id'])
 
     zipped_file = zip_model_output(input_dir, output_dir)
 
     # clean up files not used
-    delete_output_files(input_dir)
+    delete_output_files(input_dir, model_id)
 
 
 # hydro_model_builder needs to specify path where templates are,
 # what file to save output file to,
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
